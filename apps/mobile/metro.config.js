dce2845b14598ed106eec90e8bc33ad3f960950c
Felipe Brito <Felipe.brito.developer@gmail.com>
2026-04-25 12:22:52 -0300

    fix build files

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,4 +1,20 @@
+/**
+ * Polyfill for Node 18 compatibility with Metro 0.83.5+
+ * ES2023 Array methods are required by recent Metro versions
+ */
+if (!Array.prototype.toReversed) {
+	Array.prototype.toReversed = function () {
+		return [...this].reverse();
+	};
+}
+if (!Array.prototype.toSorted) {
+	Array.prototype.toSorted = function (compareFn) {
+		return [...this].sort(compareFn);
+	};
+}
+
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+
 const path = require("node:path");
 
 const projectRoot = __dirname;
